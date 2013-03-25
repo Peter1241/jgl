326d0776c69ced4b91e65200c51ae6b147647369
Justin Stoecker <justin@cs.miami.edu>
2013-03-25 03:20:11 -0400

    Removed primitives (these are called geometry now)

--- src/jgl/scene/primitives/ConePrimitive.java
+++
@@ -1,9 +0,0 @@
-/*******************************************************************************
- *  Copyright (C) 2013 Justin Stoecker
- *  The MIT License. See LICENSE in project root.
- *******************************************************************************/
-package jgl.scene.primitives;
-
-public class ConePrimitive {
-
-}
